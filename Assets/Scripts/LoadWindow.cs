6ed56144fe4e58fffe33f3492ea03ed09fa370d1
Nicolias <marat.khakimov.04@gmail.com>
2023-04-21 23:44:48 +0500

    Create loading animation

--- Assets/Scripts/LoadWindow.cs
+++ Assets/Scripts/LoadWindow.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,14 +8,68 @@ public class LoadWindow : MonoBehaviour
     [SerializeField] private float _loadTime;
     [SerializeField] private AudioServise _audioServise;
 
+    [SerializeField] private float _moveSpeed, _ampletude, _frequence;
+
+    [SerializeField] private Transform _startPosition, _endPosition;
+    [SerializeField] private GameObject _plane, _linePrefab;
+    [SerializeField] private float _rotationSpeed;
+
+    [SerializeField] private float _spawnLineFrequence;
+
+    [SerializeField] private float _upEngel, _downEngel;
+
+    private float _currentTime, _offset, _previesOffset;
+    private float _nextSpawnLineTime;
+
+    private List<GameObject> _spawnedLineCollection = new();
+
     private void Start()
     {
+        _plane.transform.position = _startPosition.position;
+
         StartCoroutine(WaitUntilLoadScene());
+    }
+
+    private void Update()
+    {
+        _currentTime += Time.deltaTime;
+
+        _offset = -(_ampletude * Mathf.Sin(_currentTime * _frequence));
+        Quaternion direction = _offset > _previesOffset ? Quaternion.Euler(0, 0, _upEngel) : Quaternion.Euler(0, 0, _downEngel);
+
+        if (Math.Round(_offset, 2) == Math.Round(_previesOffset, 2) && Mathf.Abs(_offset) > Mathf.Abs(_previesOffset))
+            direction = Quaternion.Euler(0, 0, 0);
+
+        _plane.transform.position = new Vector3(_plane.transform.position.x + _moveSpeed * Time.deltaTime, _startPosition.position.y + _offset);
+        _plane.transform.rotation = Quaternion.Slerp(_plane.transform.rotation, direction, _rotationSpeed * Time.deltaTime);
+
+        _previesOffset = _offset;
+
+        DrowLine(direction);
+    }
+
+    private void DrowLine(Quaternion direction)
+    {
+        if (Time.time >= _nextSpawnLineTime)
+        {
+            var line = Instantiate(_linePrefab);
+            line.transform.position = _plane.transform.position;
+            line.transform.rotation = Quaternion.Slerp(_plane.transform.rotation, direction, _rotationSpeed * Time.deltaTime);
+            _spawnedLineCollection.Add(line);
+
+            _nextSpawnLineTime = Time.time + _spawnLineFrequence;
+        }
     }
 
     private IEnumerator WaitUntilLoadScene()
     {
-        yield return new WaitForSeconds(_loadTime);
+        while (_plane.transform.position.x < _endPosition.position.x)
+        {
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        foreach (var item in _spawnedLineCollection)
+            Destroy(item.gameObject);
 
         gameObject.SetActive(false);
         _audioServise.BackGroundSound.Play();
